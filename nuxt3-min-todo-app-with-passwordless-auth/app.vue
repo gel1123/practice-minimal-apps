67966b2c35c266258c80d2217aeca114c12b3da8
shigeru.hyodo <41170561+gel1123@users.noreply.github.com>
2025-03-19 19:26:12 +0900

    hono rpc template

--- nuxt3-min-todo-app-with-passwordless-auth/app.vue
+++ nuxt3-min-todo-app-with-passwordless-auth/app.vue
@@ -1,5 +1,27 @@
+<script setup lang="ts">
+import type { App } from "./server/api/[...]";
+import { hc } from "hono/client";
+
+const baseUrl = useRequestURL();
+const client = hc<App>(baseUrl.origin);
+
+const { data, status, error, refresh } = useAsyncData(() =>
+  client.api.hello[":name"]
+    .$get({ param: { name: "sig" } })
+    .then((res) => res.json()),
+);
+watchEffect(() => {
+  console.log(`
+    data: ${data.value?.message},
+    status: ${status.value},
+    error: ${error.value?.message},
+  `);
+});
+</script>
+
 <template>
   <div>
+    <button @click="() => refresh()">refresh</button>
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
   </div>
